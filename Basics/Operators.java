c2e9b207372f7f2e60e3911ffaf9f692fed6ac5d
Vipul <goyalvipul2002@gmail.com>
2024-09-29 19:32:55 +0530

    Initial Commit

---
+++ Basics/Operators.java
@@ -0,0 +1,9 @@
+public class Operators {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("Your output is:" + c);
+    }
+    
+}
