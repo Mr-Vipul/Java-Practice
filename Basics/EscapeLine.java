5a6e22b7e348c27bbc32a2a93dae76a088687c36
Vipul <goyalvipul2002@gmail.com>
2024-09-30 13:03:20 +0530

    15 commit

--- Basics/EscapeLine.java
+++
@@ -1,6 +0,0 @@
-public class EscapeLine {
-    public static void main(String[] args){
-        System.err.println(89 + "\n" + 98);
-    }
-    
-}
--- Basics/TernaryOperator.java
+++
@@ -1,9 +0,0 @@
-public class TernaryOperator {
-    public static void main(String[] args) {
-        int a = 10 ;
-        int b = 20 ;
-        int max = a>b ? a : b;
-        System.out.println("Max value is " + max);
-    }
-    
-}
