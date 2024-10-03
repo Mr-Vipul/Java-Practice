d04b0aab177e9f2342972f6022c3eb831920d521
Vipul <goyalvipul2002@gmail.com>
2024-10-03 17:42:46 +0530

    23rd commit

--- 08-ArrayList/Array2DList.java
+++ 08-ArrayList/Array2DList.java
@@ -15,7 +15,6 @@ public class Array2DList {
         for(int i = 0; i<3;i++){
             for(int j=0; j<3; j++ ){
                 list.get(i).add(in.nextInt());
-
             }
         }
         
