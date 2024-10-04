6a224321303b9256aefde44edf719ab99575f0c2
Vipul <goyalvipul2002@gmail.com>
2024-10-04 23:41:30 +0530

    25th commit

--- 08-ArrayList/Array2DList.java
+++ 08-ArrayList/Array2DList.java
@@ -6,7 +6,7 @@ public class Array2DList {
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
 
-        Scanner in = new Scanner(System.in);
+        Scanner intt = new Scanner(System.in);
 
         for(int i =0; i<3;i++){
             list.add(new ArrayList<>());
@@ -14,7 +14,7 @@ public class Array2DList {
 
         for(int i = 0; i<3;i++){
             for(int j=0; j<3; j++ ){
-                list.get(i).add(in.nextInt());
+                list.get(i).add(intt.nextInt());
             }
         }
         
