40a6f0d8f7e7abd8836705ef3a797a7f5b64d3cf
Vipul <goyalvipul2002@gmail.com>
2024-10-09 23:21:09 +0530

    new commit

--- 08-ArrayList/ArrayLists.java
+++ 08-ArrayList/ArrayLists.java
@@ -6,10 +6,12 @@ public class ArrayLists {
         // Create an ArrayList
         ArrayList<Integer> list = new ArrayList<Integer>();
 
+        // okay
         list.add(1);
         System.out.println(list.get(0));
         list.remove(0);
         System.out.println(list);
+        System.out.println("okay")
     }
     
 }
